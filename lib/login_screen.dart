b683d3aee17fc67e8dbedd5f9d5ec4997c113aac
meng <rommelbenamiradavid@gmail.com>
2025-07-18 03:02:59 +0800

    Added controller for username and password

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,35 +1,59 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController _usernameController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      appBar: AppBar(title: const Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
             ),
             const SizedBox(height: 16.0),
-            const TextField(
+            TextField(
+              controller: _passwordController,
               obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
+              decoration: const InputDecoration(labelText: 'Password'),
             ),
             const SizedBox(height: 32.0),
             ElevatedButton(
               onPressed: () {
-                debugPrint('ASDASDASDASD CLICKED');
+                debugPrint('');
+                debugPrint(
+                  'ASDASDASDASD Username: ${_usernameController.text}',
+                );
+                debugPrint(
+                  'ASDASDASDASD Password: ${_passwordController.text}',
+                );
               },
               child: const Text('Login'),
             ),
